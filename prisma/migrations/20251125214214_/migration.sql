316d71397d4c17988edce49898843c3b09033112
diego rojas <devup2332@gmail.com>
2025-11-25 16:53:02 -0500

    master: Edited schema master file

---
+++ prisma/migrations/20251125214214_/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "public"."Tenant" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Tenant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tenant_name_key" ON "public"."Tenant"("name");
